c0957c8fa200502f357540b3a2aec0ab36577ded
Bill Baumgartner <bill.baumgartner@gmail.com>
2020-03-13 15:00:05 -0600

    add dedicated node pool for oger services

--- services/gke/main.tf
+++ services/gke/main.tf
@@ -137,6 +137,68 @@ resource "google_container_node_pool" "node_pool" {
   }
 }
 
+
+# -----------------------------------------------------------------------------
+# CREATE A NODE POOL FOR OGER
+# -----------------------------------------------------------------------------
+
+resource "google_container_node_pool" "node_pool_oger" {
+  provider = google-beta
+
+  name     = "private-oger-pool-1"
+  project  = var.project
+  location = var.location
+  cluster  = module.gke_cluster.name
+
+  initial_node_count = "1"
+
+  autoscaling {
+    min_node_count = "0"
+    max_node_count = "15"
+  }
+
+  management {
+    auto_repair  = "true"
+    auto_upgrade = "true"
+  }
+
+  node_config {
+    image_type   = "COS"
+    machine_type = "n1-standard-2" # 7.5 GB RAM
+
+    labels = {
+      private-oger-pool = "true"
+    }
+
+    # Add a private tag to the instances. See the network access tier table for full details:
+    # https://github.com/gruntwork-io/terraform-google-network/tree/master/modules/vpc-network#access-tier
+    tags = [
+      data.terraform_remote_state.vpc.outputs.vpc_network.private,
+      "private-oger-pool-1",
+    ]
+
+    disk_size_gb = "30"
+    disk_type    = "pd-standard"
+    preemptible  = false
+
+    service_account = module.gke_service_account.email
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform",
+    ]
+  }
+
+  lifecycle {
+    ignore_changes = [initial_node_count]
+  }
+
+  timeouts {
+    create = "30m"
+    update = "30m"
+    delete = "30m"
+  }
+}
+
 # -----------------------------------------------------------------------------
 # CREATE A GPU NODE POOL
 # -----------------------------------------------------------------------------
